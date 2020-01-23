40f2763f075c8386f3a2758e115f08bdd1f0de89
Solomon Mashat Emmanuel <semashat@gmail.com>
2020-01-23 12:11:19 +0100

    Updates

---
+++ precache-manifest.0b72b0341b785d554e7800f6052d3d13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46b4dea11edb3f9f26d82b3f9ea4fab",
+    "url": "/sme007/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "365ccea99dec4ebf92fb",
+    "url": "/sme007/monsters-rolodex/static/css/main.8faf8c3d.chunk.css"
+  },
+  {
+    "revision": "55ee059b2988e93ca287",
+    "url": "/sme007/monsters-rolodex/static/js/2.279358c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sme007/monsters-rolodex/static/js/2.279358c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "365ccea99dec4ebf92fb",
+    "url": "/sme007/monsters-rolodex/static/js/main.dbdb36d4.chunk.js"
+  },
+  {
+    "revision": "45d29bce0c8d8223c697",
+    "url": "/sme007/monsters-rolodex/static/js/runtime-main.5c1ba03d.js"
+  }
+]);
--- src/component/search-box/search-box.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './search-box.style.css';
-
-export const SearchBox = ({ placeholder, handleChange }) => (
-    <input className= "search"type= 'search' 
-    placeholder="search monster" 
-    onChange={handleChange}/>
-)
